aec4acf4b45d01b2d468a6b2270c33dded322204
Pavel Sviridov <pavlentiysv@gmail.com>
2023-04-01 20:10:08 -0400

    Add font

--- src/App.vue
+++ src/App.vue
@@ -48,15 +48,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+
 .card {
 	width: 200px;
 	height: 200px;
 	margin: 100px;
-
 }
 
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'Chakra Petch', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
